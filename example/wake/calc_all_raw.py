f9215ae5ec86768b4b39e01a38a2584ef4155c4e
awaldm <mail@awaldmann.com>
2022-04-19 23:44:05 +0200

    added code for POD, including example in calc_all_raw.py

--- example/wake/calc_all_raw.py
+++ example/wake/calc_all_raw.py
@@ -69,6 +69,9 @@ def get_rawdata(case_name, plane_name, case_type):
     return wake
 
 
+
+
+
 ######################################################################
 if __name__ == "__main__":
      # We sometimes need to create and destroy a figure BEFORE loading any Tecplot data
@@ -112,15 +115,15 @@ if __name__ == "__main__":
     wake.save_means()
 
     # interpolate mean velocity onto a regular grid
-    xi, zi, ui = ex.interpolate_struct_data(wake.x, wake.z, wake.stats.mean['u'])
+    #xi, zi, ui = ex.interpolate_struct_data(wake.x, wake.z, wake.stats.mean['u'])
 
     # get wake minimum position
-    wake_min_pos = ex.wake_min_pos(xi,zi,ui)
-    print(xi)
-    plt.plot(xi, wake_min_pos)
+    #wake_min_pos = ex.wake_min_pos(xi,zi,ui)
+    #print(xi)
+    #plt.plot(xi, wake_min_pos)
 
     # fit a rough linear approximation of the wake centerline
-    _, fit_line = ex.fit_line(xi, wake_min_pos)
+    #_, fit_line = ex.fit_line(xi, wake_min_pos)
 
     #plt.plot(xi, fit_line)
     #plt.show()
@@ -129,11 +132,11 @@ if __name__ == "__main__":
     #wake.save_rstresses(wake.stats.rs.as_dict())
 
     # get the anisotropy data including the barycentric coordinates
-    wake.compute_anisotropy(do_save = True)
+    #wake.compute_anisotropy(do_save = True)
 
-    from plot_anisotropy import draw_barycentric
+    #from plot_anisotropy import draw_barycentric
     # draw anisotropy along centerline in barycentric coordinates
-    draw_barycentric(wake.stats.an.C, wake.stats.an.xb, wake.stats.an.yb, wake.x, wake.z, xi, wake_min_pos)
+    #draw_barycentric(wake.stats.an.C, wake.stats.an.xb, wake.stats.an.yb, wake.x, wake.z, xi, wake_min_pos)
 
     #wake.compute_independent_samples(do_save = True)
     #wake.compute_PSD([], do_save = True)
@@ -141,3 +144,39 @@ if __name__ == "__main__":
 
 
     #res.save_anisotropy()
+
+    ## Run POD
+    from flowpost.wake.helpers.modal import POD_utils, modal_utils
+
+    # Set the number of modes to keep
+    num_modes = 10
+
+
+
+    # Set what to feed into POD algorithm, i.e. one or multiple variables
+
+    # Get the fluctuations
+    uflu = wake.vel.u - wake.stats.mean['u']
+    vflu = wake.vel.v - wake.stats.mean['v']
+    wflu = wake.vel.w - wake.stats.mean['w']
+
+    indata = np.vstack((uflu,vflu,wflu))
+
+    # Run POD via own hand-coded numpy implementation
+    modes, eigvals, eigvecs, coeffs = POD_utils.compute_POD(indata, num_modes)
+
+    # TODO: get this from somewhere else
+    varnames = ['u','v','w']
+
+    # Write modes to Tecplot binary files
+    newvar = modal_utils.unstack_mode(modes, varnames)
+    for i in range(num_modes):
+        POD_utils.write_mode_plt(newvar, wake.dataset, i, wake.param.res_path, 'mode')
+
+    # Create time vector for the POD coefficients.
+    POD_time = np.linspace(wake.param.start_t,wake.vel.n_samples*wake.param.dt + wake.param.start_t, wake.vel.n_samples,endpoint=True)
+
+    POD_utils.write_POD_coeffs(wake.param.res_path, case_name, POD_time, coeffs[:,0:num_modes])
+    POD_utils.write_POD_eigvals(wake.param.res_path, case_name, eigvals)
+
+
