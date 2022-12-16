Fixed anisotropy computation, added helpers for structural grid creation

src/flowpost/stats.py
@@ -42,13 +42,18 @@ class VelocityStatistics(Statistics):
         self.rs.calc_rstresses(vel.u, vel.v, vel.w)
 
     def compute_anisotropy(self, vel = None, do_save = False):
+        """Compute the anisotropy related quantities: the tensor itself and the invariants
+        """
+        
         #self.stats.atensor = AnisotropyData
+
+        """
         if self.rs.is_empty():
             self.rs = ReynoldsStresses()
             self.rs.calc_rstresses(vel.u,vel.v,vel.w)
             self.rs.kt = 0.5* (self.rs.uu + self.rs.vv + self.rs.ww)
+        """    
         # Compute the anisotropy tensor
-        print(self.rs.kt)
         a_uu, a_vv, a_ww, a_uv, a_uw, a_vw = self.an.compute_atensor(self.rs.uu, \
         self.rs.vv, \
         self.rs.ww, \
@@ -57,8 +62,8 @@ class VelocityStatistics(Statistics):
         self.rs.vw, \
         self.rs.kt)
         # Compute second and third invariants of the anisotropy tensor
-        atensor = {'uu': a_uu, 'vv': a_vv, 'ww': a_ww, 'uv': a_uv, 'uw': a_uw, 'vw': a_vw}
-
+        #atensor = {'uu': a_uu, 'vv': a_vv, 'ww': a_ww, 'uv': a_uv, 'uw': a_uw, 'vw': a_vw}
+        #self.an.set_values(a_uu, a_vv, a_ww, a_uv, a_uw, a_vw)
         self.an.invar2, self.an.invar3, self.an.ev = self.an.compute_anisotropy_invariants(a_uu, a_vv, a_ww, a_uv, a_uw, a_vw)
         # Compute barycentric coordinates
         self.an.C, self.an.xb, self.an.yb = self.an.compute_anisotropy_barycentric(self.an.ev)
@@ -83,17 +88,34 @@ class AnisotropyTensor():
         self.vw = a_vw
 
 
+    
+    def lumley_map_boundaries(self):
+        #rechts: axisymmetry
+        Jx1=np.linspace(0,2./9.,300)
+        Jy1=(3./2.) * (Jx1 *(4./3.))**(2./3.)
+        #% links: axisymmetry
+        Jx2=np.linspace(0,-1./36.,50)
+        Jy2=3./2. * (-Jx2  *4.0/3.0)**(2./3.)
+        #% oben: 2-component-turbulence
+        Jx3=np.linspace(-1./36.,2./9.,300)
+        Jy3=2./9. + Jx3*2
+        return Jx1, Jy1, np.flipud(Jx2), np.flipud(Jy2), Jx3, Jy3
+
+
+      
+
 
     def compute_atensor(self,uu, vv, ww, uv, uw, vw, kt, return_tensor = False):
         '''
         anisotropy tensor from reynolds stress tensor components
         '''
-        a_uu = np.divide(uu, (2.*kt)) - (1/3)
-        a_vv = np.divide(vv, (2.*kt)) - (1/3)
-        a_ww = np.divide(ww, (2.*kt)) - (1/3)
-        a_uv = np.divide(uv, (2.*kt))
-        a_vw = np.divide(vw, (2.*kt))
-        a_uw = np.divide(uw, (2.*kt))
+        # The nan_to_num is necessary to catch places where kt=0 to avoid division by zero errors
+        a_uu = np.nan_to_num(np.divide(uu, (2.*kt))) - (1/3)
+        a_vv = np.nan_to_num(np.divide(vv, (2.*kt))) - (1/3)
+        a_ww = np.nan_to_num(np.divide(ww, (2.*kt))) - (1/3)
+        a_uv = np.nan_to_num(np.divide(uv, (2.*kt)))
+        a_vw = np.nan_to_num(np.divide(vw, (2.*kt)))
+        a_uw = np.nan_to_num(np.divide(uw, (2.*kt)))
         if return_tensor:
             return self.atensor_components_to_array(a_uu, a_vv, a_ww, a_uv, a_uw, a_vw)
         else:
@@ -108,6 +130,7 @@ class AnisotropyTensor():
         transposition of points to dim 0 is necessary to parallelize eigenvalue computation
         rationale: np.linalg.eig computes eigenvalues
         '''
+        print(a_uu.shape)
         atensor = np.array([[a_uu, a_uv, a_uw],[a_uv, a_vv, a_vw], [a_uw, a_vw, a_ww]])
         atensor = np.squeeze(atensor)
         return np.transpose(atensor, [2,0,1])
@@ -119,7 +142,6 @@ class AnisotropyTensor():
 
         atensor = self.atensor_components_to_array(a_uu, a_vv, a_ww, a_uv, a_uw, a_vw)
         print('shape of atensor: ' + str(atensor.shape))
-
         ev, _ = np.linalg.eig(atensor)
         ev = np.transpose(ev, [1,0])
         for i in range(ev.shape[1]):
@@ -160,16 +182,27 @@ class ReynoldsStresses():
     '''
 
     '''
+    """
+    @property
+    def kt(self):
+        if self._kt.any() == None:
+            self._kt = 0.5* (self.uu + self.vv + self.ww)
+        return self._kt
 
+    @kt.setter
+    def kt(self, a):
+        self._kt = a
+    """
     def __init__(self):
         # initialize defaults
         #VelocityStatistics.__init__(self)
-        self.uu: np.ndarray = None
-        self.uv: np.ndarray = None
-        self.uw: np.ndarray = None
-        self.vw: np.ndarray = None
-        self.uv: np.ndarray = None
-        self.vw: np.ndarray = None
+        self.uu: np.ndarray = np.array([])
+        self.uv: np.ndarray = np.array([])
+        self.uw: np.ndarray = np.array([])
+        self.vw: np.ndarray = np.array([])
+        self.uv: np.ndarray = np.array([])
+        self.vw: np.ndarray = np.array([])
+        #self._kt: np.ndarray = np.array([])
 
     def is_empty(self):
         return self.uu == None
@@ -232,3 +265,4 @@ class ReynoldsStresses():
             self.uv.reshape(newshape)
             self.uw.reshape(newshape)
             self.vw.reshape(newshape)
+        self.kt = 0.5* (self.uu + self.vv + self.ww)

src/flowpost/wake/data.py
@@ -166,7 +166,8 @@ class WakeField():
         # Call the Tecplot dataset rotation function
         print(self.x_PMR)
         print(self.z_PMR)
-        wt.rotate_dataset(self.dataset, self.x_PMR, self.z_PMR, aoa)
+        if self.dataset is not None:
+            wt.rotate_dataset(self.dataset, self.x_PMR, self.z_PMR, aoa)
         x_WT, z_WT = wt.transform_wake_coords(self.x, self.z, self.x_PMR, self.z_PMR, aoa)
         u_WT, w_WT = wt.rotate_velocities(self.vel.u, self.vel.v, self.vel.w, self.x_PMR, self.z_PMR, aoa)
         self.vel.u = u_WT
@@ -192,11 +193,36 @@ class WakeField():
             self.save_rstresses(self.rstresses, res_path = self.param.res_path, file_prefix = self.param.case_name+'_'+ self.param.plane_name)
         """
 
+    def interpolation_grid(self, xlim, zlim, xsize, zsize):
+        """Helper function to obtain a structured 2D interpolation target grid in a given region 
+
+        :param xlim: _description_
+        :param zlim: _description_
+        :param xsize: _description_
+        :param zsize: _description_
+        :return: _description_
+        """
+        x0, z0 = xlim[0], zlim[0]
+        x1, z1 = xlim[1], zlim[1]
+
+        xi, zi = np.linspace(x0, x1, xsize), np.linspace(z0, z1, zsize)
+        xmesh, zmesh = np.meshgrid(xi,zi)
+        return xi, zi, xmesh, zmesh
+
+
+    def interpolate_struct_field(self, coords = None, var = None, xlim = None, zlim = None, xsize = 100, zsize = 250):
+        if coords is None:
+            x = self.x
+            z = self.z
+
+        xi, zi, xmesh, zmesh = self.interpolation_grid(xlim, zlim, xsize, zsize)
+        return xi, zi, scipy.interpolate.griddata((x, z), var, (xmesh, zmesh), method='linear')
+
+
 
     def interpolate_struct(self, coords = None, variables = ['u', 'v', 'w'], xlim = None, zlim = None, in_data = None, xsize = 100, zsize = 250):
         """Interpolate to a structured 2D dataset
     
-
 
         """
         if coords is None:
@@ -209,27 +235,30 @@ class WakeField():
             u = self.vel.u
             v = self.vel.v
             w = self.vel.w
+        #else:
+
 
         if xlim is None:
             xlim = (1.05, 1.65)
         if zlim is None:
             zlim = (-0.05, 0.25)
             
-        x0, z0 = xlim[0], zlim[0]
-        x1, z1 = xlim[1], zlim[1]
+        xi, zi, xmesh, zmesh = self.interpolation_grid(xlim, zlim, xsize, zsize)
 
-        xi, zi = np.linspace(x0, x1, xsize), np.linspace(z0, z1, zsize)
-        xmesh, zmesh = np.meshgrid(xi,zi)
 
         print('shape of xi: ' + str(xi.shape))
         print('shape of zi: ' + str(zi.shape))
         print('mesh shape: ' + str(xmesh.shape))
+
         struct = {}
+        struct['xi']  = xi
+        struct['zi']  = zi
+        struct['xmesh'] = xmesh
+        struct['zmesh'] = zmesh
         for var in variables:
             #self.struct[var] = scipy.interpolate.griddata((x_WT[case], z_WT[case]), u_WT[case], (xmesh, zmesh), method='cubic')
             #print(self.vel.get_var(var))
-            struct['xi']  = xi
-            struct['zi']  = zi
+
             struct[var]  = scipy.interpolate.griddata((x, z), self.vel.get_var(var), (xmesh, zmesh), method='linear')
 
         return struct
@@ -367,6 +396,8 @@ class WakeField():
     def save_plt_file(self, save_var, filename):
         tecreader.save_plt(save_var, self.dataset, filename, addvars = True, removevars = True)
         
+    #def save_numpy(self, save_var, filename):
+    #    np.savez(filename)
 
 
     def compute_wake_quants(self):